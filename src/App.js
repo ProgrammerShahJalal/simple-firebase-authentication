2174b7db20778bb71e50961fc1a5fec298fe4105
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-10-06 19:17:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from 'firebase/auth'
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { useState } from 'react';
+
+
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+const gitHubprovider = new GithubAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({})
+
+  const auth = getAuth();
+
+  const handleGitHubSignIn = () => {
+    signInWithPopup(auth, gitHubprovider)
+      .then(result => {
+        console.log(result.user)
+        const { displayName, email, photoURL } = result.user;
+        const logginUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logginUser);
+      })
+  }
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        console.log(result.user);
+        const { displayName, email, photoURL } = result.user;
+        const logginUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(logginUser);
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+    setUser({})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!user.name ?
+        <div>
+          <button onClick={handleGoogleSignIn}>Sing in Google</button>
+          <button onClick={handleGitHubSignIn}>GitHub sign in</button>
+        </div> :
+        <button onClick={handleSignOut}>Sign Out</button>
+      }
+      <br />
+      {
+        user.name && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address: {user.email}</p>
+          <p>This is you</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
